a2c609cffb2fcfca29139b78e89d6b414040bd3d
Bartosz Głąb <bartoszzawiercie@gmail.com>
2022-11-08 01:19:47 +0100

    calculator

---
+++ precache-manifest.f9789e5c2d7b24a81c75e464f094b402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf85bd449917ae595a39",
+    "url": "/static/css/main.d94f2bab.chunk.css"
+  },
+  {
+    "revision": "bf85bd449917ae595a39",
+    "url": "/static/js/main.1dc4cd9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c0ba17f1dece9f6aa68",
+    "url": "/static/js/2.2626944e.chunk.js"
+  },
+  {
+    "revision": "300924f4b01a7e368ccaeeb2d920c051",
+    "url": "/static/media/piggy-big.300924f4.jpg"
+  },
+  {
+    "revision": "9c51452be0c592840ecd7b32bb2532c9",
+    "url": "/index.html"
+  }
+];
